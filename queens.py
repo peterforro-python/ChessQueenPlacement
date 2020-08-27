83c536589d92d47b40473ee3c5f4774dc70fbfa7
Peter Forro <pforro@wfscorp.com>
2020-08-27 20:43:56 +0200

    chessboard has been implemented

--- queens.py
+++ queens.py
@@ -1,9 +1,18 @@
+from os import system
+
 class QueenPlacement:
-    def __init__(self):
-        pass
+    def __init__(self) -> None:
+        self.width = 8
+        self.height = self.width
+        self.board = [["W" if (x + y) % 2 == 0 else "B" for x in range(self.width)] for y in range(self.height)]
+
+    def __str__(self) -> str:
+        return "\n".join([" ".join(row) for row in self.board])
 
 
 
 
 if __name__ == "__main__":
-    pass
+    system("cls")
+    task = QueenPlacement()
+    print(task)
